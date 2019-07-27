65918b1f6a7abb6f54c58b3566978302db80648b
Sergioamjr <sergioamjr91@gmail.com>
2019-07-27 21:28:58 +0100

    Add netlify domain into cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://save-notes.netlify.com"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "x-auth"],
   optionsSuccessStatus: 200
