ef3b614b5f09fc196cbe3076507a35dc89f0b987
Sérgio Júnior <sergioamjr91@gmail.com>
2021-02-25 16:07:21 -0300

    Updated cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,8 @@
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://save-notes.netlify.com"],
+  origin: ["http://localhost:3000", "https://save-notes.netlify.app"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "x-auth"],
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
